071546f95168a27561dad01fd0a4a0733f635fb8
houxinchao <houxinchao@jd.com>
2023-06-16 11:29:49 +0800

    test: 测试功能

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,52 @@
-/*
- * @Autor: ERP
- * @Email: 邮箱
- * @Description: 
- * @CreateDate: Do not edit
- * @LastEditors: houxinchao
- */
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as fs from 'fs'
+import * as vscode from 'vscode'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "AutoMatchFormat" is now active!');
+  console.log('插件被激活')
+  // 注册命令
+  let disposable = vscode.commands.registerCommand(
+    'AutoMatchFormat',
+    (uri) => {
+      vscode.window.showInformationMessage(
+        'Hello VS Code from AutoMatchFormat!'
+      )
+      // 文件路径
+      const filePath = uri.path.substring(1)
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          vscode.window.showErrorMessage(`获取文件时遇到错误了${err}!!!`)
+        }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('AutoMatchFormat.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code from AutoMatchFormat!');
-	});
+        if (stats.isDirectory()) {
+          vscode.window.showWarningMessage(
+            `检测的是文件夹，不是文件，请重新选择！！！`
+          )
+        }
 
-	context.subscriptions.push(disposable);
+        if (stats.isFile()) {
+          const size = stats.size
+          const createTime = stats.birthtime.toLocaleString()
+          const modifyTime = stats.mtime.toLocaleString()
+
+          vscode.window.showInformationMessage(
+            `
+                        文件大小为:${size}字节;
+                        文件创建时间为:${createTime};
+                        文件修改时间为:${modifyTime}
+                    `,
+            { modal: true }
+          )
+        }
+      })
+    }
+  )
+  // 将命令放入上下文对象，使其生效
+  console.log('上下文环境', context)
+  context.subscriptions.push(disposable)
 }
 
 // this method is called when your extension is deactivated
